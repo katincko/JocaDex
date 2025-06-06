feba01247c57b60531280444d385170599b86d54
Joaquim Alexander Inácio Costa <RES0072862@aluno.firjansenaisesi.com.br>
2025-06-05 21:41:33 -0300

    iniciando projeto

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
+const pokemonList = document.getElementById("lista-pokemon")
+
+
+
+function converter(pokemon){
+   
+    return `<li class="pokemon">
+                <p class="${pokemon}">${pokemon.name}</p>
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </li>
+            `
+}
+
+function pokeDetails(pokemon){
+    console.log(pokemon)
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonresponse) => jsonresponse.results)
+    .then((list) => list.map(pokeDetails))
+    .then((details) => Promise.all(details))
+    .then((newList) => pokemonList.innerHTML = newList.map(converter).join(""))
+    .catch((error) => console.log(error))
+    
